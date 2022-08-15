80c3558a849e682cddd06d486dcb897c568c83ce
Dan <dan.baghilovici@gmail.com>
2022-08-15 21:29:02 +0300

    update

---
+++ copy-styles.sh
@@ -0,0 +1,4 @@
+mkdir -p themes/protocol/static && cp -r modules/protocol/dist/protocol/css "$_"
+mkdir -p themes/protocol/static && cp -r modules/protocol/dist/protocol/fonts "$_"
+mkdir -p themes/protocol/static && cp -r modules/protocol/dist/protocol/img "$_"
+mkdir -p themes/protocol/static && cp -r modules/protocol/dist/protocol/js "$_"
